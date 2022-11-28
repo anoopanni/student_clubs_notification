8cf55acdcb23b55554401486c2c84de1a7cb93db
anoopanni <anoop.shivayogi@gmail.com>
2022-11-28 03:18:07 +0000

    Added subscriber_id

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template
+from pubsub import PubSub
 import os
+
 
 app = Flask(__name__, static_url_path='/static')
 
+subscriber_id = -1
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    global subscriber_id
+    subscriber_id += 1
+    return render_template('index.html', subscriber_id=subscriber_id)
+
 
 
 if __name__ == "__main__":
